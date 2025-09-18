7613106deb67ac289460ddff1ea8761fb46b53f0
Sergio Daniel Betancur <pladedcolnasa@gmail.com>
2025-09-17 21:40:47 -0500

    Agregado modelo Repuesto.java con atributos y getters/setters

---
+++ src/main/java/com/danimotors/model/Repuesto.java
@@ -0,0 +1,52 @@
+package com.danimotors.model;
+
+public class Repuesto {
+    private int id;
+    private String nombre;
+    private double precio;
+    private int cantidad;
+
+    // Constructor vacío
+    public Repuesto() {}
+
+    // Constructor con parámetros
+    public Repuesto(int id, String nombre, double precio, int cantidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}
